52182b116cbf6689fd95e06f5e739f4b488c0a47
Max Zang <yzang2@ualberta.ca>
2024-10-26 16:28:57 -0600

    create basic user model (without profile pic)

---
+++ app/src/main/java/com/example/marill_many_events/User.java
@@ -0,0 +1,18 @@
+package com.example.marill_many_events;
+
+public class User {
+    public String name;
+    public String email;
+    public int phone;
+
+    public User() {
+        // Default constructor
+    }
+
+    public User(String name, String email, int phone) {
+        // Initialize user profile
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
+}
