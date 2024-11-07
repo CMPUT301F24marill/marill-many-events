8578dfd4183a9ab2b7574de01dc05d1adda79add
Max Zang <yzang2@ualberta.ca>
2024-11-06 21:49:43 -0700

    Add user status to user model.
    Create CreateFacilityFragment.

---
+++ app/src/main/java/com/example/marill_many_events/fragments/CreateFacilityFragment.java
@@ -0,0 +1,12 @@
+package com.example.marill_many_events.fragments;
+
+import androidx.fragment.app.Fragment;
+
+/**
+ * CreateFacilityFragment represents a fragment that allow non-organizer users
+ * to create facility profile.
+ */
+public class CreateFacilityFragment extends Fragment {
+    
+
+}
