dd50ff5bb26f93e340c82b7c496756e2cc166c5f
Max Zang <yzang2@ualberta.ca>
2024-11-07 15:35:15 -0700

    implement the basic view for facility registration

--- app/src/main/java/com/example/marill_many_events/fragments/CreateFacilityFragment.java
+++ app/src/main/java/com/example/marill_many_events/fragments/CreateFacilityFragment.java
@@ -1,12 +1,49 @@
 package com.example.marill_many_events.fragments;
 
+import android.graphics.text.TextRunShaper;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import com.example.marill_many_events.R;
 
 /**
  * CreateFacilityFragment represents a fragment that allow non-organizer users
  * to create facility profile.
  */
 public class CreateFacilityFragment extends Fragment {
-    
 
+    private EditText editTextName;
+    private EditText editTextLocation;
+    private Button buttonCreate;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_create_facility, container, false);  // inflate the layout
+
+        // initialize the views
+        editTextName = view.findViewById(R.id.editTextFacilityName);
+        editTextLocation = view.findViewById(R.id.editTextFacilityLocation);
+        buttonCreate = view.findViewById(R.id.buttonCreateFacility);
+
+        // set up buttonCreate onclick listener
+        buttonCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseFacilityRegistration();
+            }
+        });
+
+
+        return view;
+    }
 }
+
