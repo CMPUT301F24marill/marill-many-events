8bd6b7edad8a137b86571c37b567398926ae7e07
Max Zang <yzang2@ualberta.ca>
2024-11-08 15:06:09 -0700

    added in the facility to the app added the recognition of device and transitions to different fragments when pressing menu

--- app/src/main/java/com/example/marill_many_events/models/Facility.java
+++ app/src/main/java/com/example/marill_many_events/models/Facility.java
@@ -3,57 +3,30 @@ package com.example.marill_many_events.models;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Facility represents a physical or virtual location where events take place.
- * It holds a list of {@link Event} objects and provides methods to manage these events
- * and the location of the facility.
- */
-public class Facility {
+public class Facility{
     private List<Event> Events;
 
-    String location;
+    String Name;
+    String Location;
 
-    /**
-     * Constructs a Facility with a specified location.
-     *
-     * @param location The location of the facility.
-     */
-    public Facility(String location){
+    public Facility(String name, String location){
         this.Events = new ArrayList<>();
-        this.location = location;
+        this.Name = name;
+        this.Location = location;
     }
 
-    /**
-     * Retrieves the location of the facility.
-     *
-     * @return The location of the facility.
-     */
     public String getLocation() {
-        return location;
+        return this.Location;
     }
 
-    /**
-     * Sets the location of the facility.
-     *
-     * @param location The new location of the facility.
-     */
     public void setLocation(String location) {
-        this.location = location;
+        this.Location = location;
     }
 
-    /**
-     * Retrieves the list of events associated with this facility.
-     *
-     * @return A {@link List} of {@link Event} objects linked to the facility.
-     */
     public List<Event> getEvents() {
         return Events;
     }
 
-    /**
-     * Clears all events associated with this facility.
-     * This method should be called when the facility is being destroyed or no longer used.
-     */
     public void onDestroy(){
         this.Events.clear();
     }
