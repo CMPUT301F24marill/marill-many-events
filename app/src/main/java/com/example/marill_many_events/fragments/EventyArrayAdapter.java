7cbd827b11991df26a21c84f15443f5da1187bbc
qiantong-guo <113124422+qiantong-guo@users.noreply.github.com>
2024-11-07 19:16:47 -0700

    Update EventyArrayAdapter.java

--- app/src/main/java/com/example/marill_many_events/fragments/EventyArrayAdapter.java
+++ app/src/main/java/com/example/marill_many_events/fragments/EventyArrayAdapter.java
@@ -21,10 +21,24 @@ import java.util.ArrayList;
 
 //EventyArrayAdapter contains Events in list and retrieves a books information for the view
 public class EventyArrayAdapter extends ArrayAdapter<Event> {
+    /**
+     * Constructor for creating an EventyArrayAdapter.
+     *
+     * @param context The context in which the adapter is running.
+     * @param events  The list of Event objects to be displayed.
+     */
     public EventyArrayAdapter(Context context, ArrayList<Event> events) {
         super(context, 0, events);
     }
 
+    /**
+     * Provides a view for an adapter view (ListView, GridView, etc.).
+     *
+     * @param position    The position of the item within the adapter's data set.
+     * @param convertView The old view to reuse, if possible.
+     * @param parent      The parent view that this view will eventually be attached to.
+     * @return A View corresponding to the data at the specified position.
+     */
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
