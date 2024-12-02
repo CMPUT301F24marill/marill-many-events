9f57e73f4e09003aa6343973f97eaab13d2746f4
qiantong-guo <113124422+qiantong-guo@users.noreply.github.com>
2024-12-02 16:10:08 -0700

    docu

--- app/src/main/java/com/example/marill_many_events/EventsCallback.java
+++ app/src/main/java/com/example/marill_many_events/EventsCallback.java
@@ -1,10 +1,27 @@
 package com.example.marill_many_events;
 
 import com.example.marill_many_events.models.Event;
-
+/**
+ * Interface for handling callback events related to Firebase operations on events.
+ */
 public interface EventsCallback {
+    /**
+     * Called when an event is successfully created in Firebase.
+     *
+     * @param documentID The document ID of the newly created event.
+     */
     void onEventCreate(String documentID);
+    /**
+     * Called when event details are retrieved from Firebase.
+     *
+     * @param event The retrieved event object.
+     */
     void getEvent(Event event);
+    /**
+     * Called when the event poster URL is successfully uploaded to Firebase Storage.
+     *
+     * @param url The download URL of the uploaded poster.
+     */
     void onPosterUpload(String url);
 
 }
--- app/src/main/java/com/example/marill_many_events/FacilityCallback.java
+++ app/src/main/java/com/example/marill_many_events/FacilityCallback.java
@@ -1,9 +1,22 @@
 package com.example.marill_many_events;
 
 import com.example.marill_many_events.models.Facility;
-
+/**
+ * Interface for handling callback events related to Firebase operations on facilities.
+ */
 public interface FacilityCallback {
+    /**
+     * Called when facility details are successfully loaded from Firebase.
+     *
+     * @param facility The loaded facility object. If the facility does not exist, this will be null.
+     */
     void onFacilityLoaded(Facility facility);
+    /**
+     * Called when facility details are successfully updated in Firebase.
+     */
     void onFacilityUpdated();
+    /**
+     * Called when facility details are successfully updated in Firebase.
+     */
     void onFacilityRegistered();
 }
